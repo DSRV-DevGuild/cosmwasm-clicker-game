51cb9196134cf092a5633c91619446e1d2357e26
Yoon-Suji <jidl88@ewhain.net>
2022-07-01 17:58:40 +0900

    [1-1] connect testnet network to keplr wallet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import "./App.css";
+import { useState } from "react";
+import networkInfo from "./wallet/network_info";
+import connectWallet from "./wallet/connect";
 
 function App() {
+  // connectWallet에서 받아올 값
+  const [client, setClient] = useState();
+  const [address, setAddress] = useState();
+  const [balance, setBalance] = useState();
+  const [chainId, setChainId] = useState();
+
+  // connectWallet 으로 전달할 함수
+  const getInfo = (client, address, balance, chainId) => {
+    setClient(client);
+    setAddress(address);
+    setBalance(balance);
+    setChainId(chainId);
+    // (debug)
+    console.log(address);
+  };
+  // connectWallet 호출하는 방법 (debug)
+  connectWallet(networkInfo["malaga-420"], { getInfo });
+
   return <div className="App"></div>;
 }
 
---
+++ src/wallet/connect.js
@@ -0,0 +1,39 @@
+// src/wallet/connect.js
+import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
+
+const connectWallet = async (chainInfo, { getInfo }) => {
+  // 사용자의 브라우저에 Keplr extension이 설치되었는지 확인
+  if (!window.getOfflineSigner || !window.keplr) {
+    alert("Please install keplr extension");
+  }
+  // Keplr wallet에 네트워크 추가
+  if (window.keplr.experimentalSuggestChain) {
+    try {
+      await window.keplr.experimentalSuggestChain(chainInfo);
+    } catch {
+      alert("Failed to suggest the chain");
+    }
+  } else {
+    alert("Please use the recent version of keplr extension");
+  }
+  // Keplr에 chainId로의 접근 요청
+  await window.keplr.enable(chainInfo.chainId);
+  // 체인 ID를 이용해서 OfflineSigner 가져오기
+  const offlineSigner = window.getOfflineSigner(chainInfo.chainId);
+  // address & public key 페어 배열 리턴
+  const accounts = await offlineSigner.getAccounts();
+  // SigningCosmWasmClient 생성
+  const client = await SigningCosmWasmClient.connectWithSigner(
+    chainInfo.rpc,
+    offlineSigner
+  );
+  // 해당 주소의 balance 가져오기
+  const balance = await client.getBalance(
+    accounts[0].address,
+    chainInfo.stakeCurrency.coinMinimalDenom
+  );
+  // 부모 컴포넌트로 값을 넘겨주기 위한 함수
+  getInfo(client, accounts[0].address, balance, chainInfo.chainId);
+};
+
+export default connectWallet;
